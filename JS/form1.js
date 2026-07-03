1e66ad49db46e4d7af2d11592d8f2fd7d26c76eb
KlJtN <145041842+KLEJBOMB@users.noreply.github.com>
2026-07-03 11:43:40 +0200

    Risoluzione bug

--- JS/form1.js
+++ JS/form1.js
@@ -5,6 +5,8 @@ const fieldForm = document.querySelector(".fieldForm")
 const emailInput = document.querySelector(".email")
 const passwordInput = document.querySelector(".password")
 const valdiationResult = document.querySelector("#validationResult")
+const errorMessageEmail = document.querySelector(".errorMessageEmail")
+const errorMessagePassword = document.querySelector(".errorMessagePassword")
 
 
 form.addEventListener("submit", function(e){
@@ -18,21 +20,23 @@ form.addEventListener("submit", function(e){
     validationData(email,password)
 })
 
-submitBtn.addEventListener("click", function(e){
-    
-})
 
 function validationData(email, password) {
     if (!email || !email.includes("@")){
         emailInput.classList.add("error")
+        errorMessageEmail.textContent = "Email non valida!"
     } else {
         emailInput.classList.remove("error")
+        errorMessageEmail.textContent = ""
+
     }
 
     if (!password || password.length < 8){
         passwordInput.classList.add("error")
+        errorMessagePassword.textContent = "Passsword non valida!"
     } else {
         passwordInput.classList.remove("error")
+        errorMessagePassword.textContent = ""
+
     }
-    return
 }
